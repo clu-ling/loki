8f1508a0f2b4f4ac9f5bd763dfb860b8fac1e192
Gus Hahn-Powell <gushahnpowell@gmail.com>
2018-02-15 12:41:40 -0700

    Antlr output for updated grammar

--- loki/LokiParser.py
+++ loki/LokiParser.py
@@ -8,38 +8,38 @@ import sys
 def serializedATN():
     with StringIO() as buf:
         buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\30")
-        buf.write("l\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b")
+        buf.write("j\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b")
         buf.write("\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r\4\16\t")
         buf.write("\16\4\17\t\17\4\20\t\20\4\21\t\21\3\2\3\2\6\2%\n\2\r\2")
         buf.write("\16\2&\3\3\3\3\3\3\3\3\5\3-\n\3\3\4\3\4\3\5\3\5\3\6\3")
-        buf.write("\6\3\6\3\7\5\7\67\n\7\3\7\3\7\3\b\3\b\3\b\5\b>\n\b\3\b")
-        buf.write("\3\b\3\t\3\t\3\t\3\n\3\n\3\n\3\n\3\13\3\13\5\13K\n\13")
-        buf.write("\3\f\3\f\3\f\3\f\3\r\3\r\3\r\3\r\3\r\5\rV\n\r\3\16\3\16")
-        buf.write("\5\16Z\n\16\3\17\6\17]\n\17\r\17\16\17^\3\20\3\20\7\20")
-        buf.write("c\n\20\f\20\16\20f\13\20\3\20\3\20\3\21\3\21\3\21\2\2")
-        buf.write("\22\2\4\6\b\n\f\16\20\22\24\26\30\32\34\36 \2\5\3\2\25")
-        buf.write("\27\3\2\26\27\3\2\r\24\2g\2$\3\2\2\2\4,\3\2\2\2\6.\3\2")
-        buf.write("\2\2\b\60\3\2\2\2\n\62\3\2\2\2\f\66\3\2\2\2\16:\3\2\2")
-        buf.write("\2\20A\3\2\2\2\22D\3\2\2\2\24J\3\2\2\2\26L\3\2\2\2\30")
-        buf.write("U\3\2\2\2\32Y\3\2\2\2\34\\\3\2\2\2\36`\3\2\2\2 i\3\2\2")
-        buf.write("\2\"%\5\16\b\2#%\5\4\3\2$\"\3\2\2\2$#\3\2\2\2%&\3\2\2")
-        buf.write("\2&$\3\2\2\2&\'\3\2\2\2\'\3\3\2\2\2(-\5\n\6\2)-\5\f\7")
-        buf.write("\2*-\5\6\4\2+-\5\b\5\2,(\3\2\2\2,)\3\2\2\2,*\3\2\2\2,")
-        buf.write("+\3\2\2\2-\5\3\2\2\2./\7\3\2\2/\7\3\2\2\2\60\61\7\4\2")
-        buf.write("\2\61\t\3\2\2\2\62\63\7\13\2\2\63\64\5\24\13\2\64\13\3")
-        buf.write("\2\2\2\65\67\7\f\2\2\66\65\3\2\2\2\66\67\3\2\2\2\678\3")
-        buf.write("\2\2\289\5\24\13\29\r\3\2\2\2:=\7\5\2\2;>\5\22\n\2<>\5")
-        buf.write("\20\t\2=;\3\2\2\2=<\3\2\2\2>?\3\2\2\2?@\7\6\2\2@\17\3")
-        buf.write("\2\2\2AB\7\7\2\2BC\5\22\n\2C\21\3\2\2\2DE\5 \21\2EF\7")
-        buf.write("\t\2\2FG\5\24\13\2G\23\3\2\2\2HK\5\26\f\2IK\5\30\r\2J")
-        buf.write("H\3\2\2\2JI\3\2\2\2K\25\3\2\2\2LM\7\b\2\2MN\5\34\17\2")
-        buf.write("NO\7\b\2\2O\27\3\2\2\2PQ\7\n\2\2QR\5\36\20\2RS\7\n\2\2")
-        buf.write("SV\3\2\2\2TV\5\36\20\2UP\3\2\2\2UT\3\2\2\2V\31\3\2\2\2")
-        buf.write("WZ\5\36\20\2XZ\5\34\17\2YW\3\2\2\2YX\3\2\2\2Z\33\3\2\2")
-        buf.write("\2[]\t\2\2\2\\[\3\2\2\2]^\3\2\2\2^\\\3\2\2\2^_\3\2\2\2")
-        buf.write("_\35\3\2\2\2`d\7\25\2\2ac\t\3\2\2ba\3\2\2\2cf\3\2\2\2")
-        buf.write("db\3\2\2\2de\3\2\2\2eg\3\2\2\2fd\3\2\2\2gh\7\25\2\2h\37")
-        buf.write("\3\2\2\2ij\t\4\2\2j!\3\2\2\2\f$&,\66=JUY^d")
+        buf.write("\6\3\6\3\7\3\7\3\7\3\b\3\b\3\b\5\b<\n\b\3\b\3\b\3\t\3")
+        buf.write("\t\3\t\3\n\3\n\3\n\3\n\3\13\3\13\5\13I\n\13\3\f\3\f\3")
+        buf.write("\f\3\f\3\r\3\r\3\r\3\r\3\r\5\rT\n\r\3\16\3\16\5\16X\n")
+        buf.write("\16\3\17\6\17[\n\17\r\17\16\17\\\3\20\3\20\7\20a\n\20")
+        buf.write("\f\20\16\20d\13\20\3\20\3\20\3\21\3\21\3\21\2\2\22\2\4")
+        buf.write("\6\b\n\f\16\20\22\24\26\30\32\34\36 \2\5\3\2\25\27\3\2")
+        buf.write("\26\27\3\2\r\24\2d\2$\3\2\2\2\4,\3\2\2\2\6.\3\2\2\2\b")
+        buf.write("\60\3\2\2\2\n\62\3\2\2\2\f\65\3\2\2\2\168\3\2\2\2\20?")
+        buf.write("\3\2\2\2\22B\3\2\2\2\24H\3\2\2\2\26J\3\2\2\2\30S\3\2\2")
+        buf.write("\2\32W\3\2\2\2\34Z\3\2\2\2\36^\3\2\2\2 g\3\2\2\2\"%\5")
+        buf.write("\16\b\2#%\5\4\3\2$\"\3\2\2\2$#\3\2\2\2%&\3\2\2\2&$\3\2")
+        buf.write("\2\2&\'\3\2\2\2\'\3\3\2\2\2(-\5\n\6\2)-\5\f\7\2*-\5\6")
+        buf.write("\4\2+-\5\b\5\2,(\3\2\2\2,)\3\2\2\2,*\3\2\2\2,+\3\2\2\2")
+        buf.write("-\5\3\2\2\2./\7\3\2\2/\7\3\2\2\2\60\61\7\4\2\2\61\t\3")
+        buf.write("\2\2\2\62\63\7\13\2\2\63\64\5\24\13\2\64\13\3\2\2\2\65")
+        buf.write("\66\7\f\2\2\66\67\5\24\13\2\67\r\3\2\2\28;\7\5\2\29<\5")
+        buf.write("\22\n\2:<\5\20\t\2;9\3\2\2\2;:\3\2\2\2<=\3\2\2\2=>\7\6")
+        buf.write("\2\2>\17\3\2\2\2?@\7\7\2\2@A\5\22\n\2A\21\3\2\2\2BC\5")
+        buf.write(" \21\2CD\7\t\2\2DE\5\24\13\2E\23\3\2\2\2FI\5\26\f\2GI")
+        buf.write("\5\30\r\2HF\3\2\2\2HG\3\2\2\2I\25\3\2\2\2JK\7\b\2\2KL")
+        buf.write("\5\34\17\2LM\7\b\2\2M\27\3\2\2\2NO\7\n\2\2OP\5\36\20\2")
+        buf.write("PQ\7\n\2\2QT\3\2\2\2RT\5\36\20\2SN\3\2\2\2SR\3\2\2\2T")
+        buf.write("\31\3\2\2\2UX\5\36\20\2VX\5\34\17\2WU\3\2\2\2WV\3\2\2")
+        buf.write("\2X\33\3\2\2\2Y[\t\2\2\2ZY\3\2\2\2[\\\3\2\2\2\\Z\3\2\2")
+        buf.write("\2\\]\3\2\2\2]\35\3\2\2\2^b\7\25\2\2_a\t\3\2\2`_\3\2\2")
+        buf.write("\2ad\3\2\2\2b`\3\2\2\2bc\3\2\2\2ce\3\2\2\2db\3\2\2\2e")
+        buf.write("f\7\25\2\2f\37\3\2\2\2gh\t\4\2\2h!\3\2\2\2\13$&,;HSW\\")
+        buf.write("b")
         return buf.getvalue()
 
 
@@ -170,7 +170,7 @@ class LokiParser ( Parser ):
                     self.state = 32
                     self.token_constraint()
                     pass
-                elif token in [LokiParser.T__0, LokiParser.T__1, LokiParser.SLASH, LokiParser.QUOTE, LokiParser.INCOMING_DEP, LokiParser.OUTGOING_DEP, LokiParser.LETTER]:
+                elif token in [LokiParser.T__0, LokiParser.T__1, LokiParser.INCOMING_DEP, LokiParser.OUTGOING_DEP]:
                     self.state = 33
                     self.graph_traversal()
                     pass
@@ -180,7 +180,7 @@ class LokiParser ( Parser ):
                 self.state = 36 
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
-                if not ((((_la) & ~0x3f) == 0 and ((1 << _la) & ((1 << LokiParser.T__0) | (1 << LokiParser.T__1) | (1 << LokiParser.OPEN_TC) | (1 << LokiParser.SLASH) | (1 << LokiParser.QUOTE) | (1 << LokiParser.INCOMING_DEP) | (1 << LokiParser.OUTGOING_DEP) | (1 << LokiParser.LETTER))) != 0)):
+                if not ((((_la) & ~0x3f) == 0 and ((1 << _la) & ((1 << LokiParser.T__0) | (1 << LokiParser.T__1) | (1 << LokiParser.OPEN_TC) | (1 << LokiParser.INCOMING_DEP) | (1 << LokiParser.OUTGOING_DEP))) != 0)):
                     break
 
         except RecognitionException as re:
@@ -240,7 +240,7 @@ class LokiParser ( Parser ):
                 self.state = 38
                 self.incoming_traversal()
                 pass
-            elif token in [LokiParser.SLASH, LokiParser.QUOTE, LokiParser.OUTGOING_DEP, LokiParser.LETTER]:
+            elif token in [LokiParser.OUTGOING_DEP]:
                 self.enterOuterAlt(localctx, 2)
                 self.state = 39
                 self.outgoing_traversal()
@@ -410,18 +410,11 @@ class LokiParser ( Parser ):
 
         localctx = LokiParser.Outgoing_traversalContext(self, self._ctx, self.state)
         self.enterRule(localctx, 10, self.RULE_outgoing_traversal)
-        self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
+            self.state = 51
+            self.match(LokiParser.OUTGOING_DEP)
             self.state = 52
-            self._errHandler.sync(self)
-            _la = self._input.LA(1)
-            if _la==LokiParser.OUTGOING_DEP:
-                self.state = 51
-                self.match(LokiParser.OUTGOING_DEP)
-
-
-            self.state = 54
             self.pattern()
         except RecognitionException as re:
             localctx.exception = re
@@ -465,23 +458,23 @@ class LokiParser ( Parser ):
         self.enterRule(localctx, 12, self.RULE_token_constraint)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 56
+            self.state = 54
             self.match(LokiParser.OPEN_TC)
-            self.state = 59
+            self.state = 57
             self._errHandler.sync(self)
             token = self._input.LA(1)
             if token in [LokiParser.WORD, LokiParser.LEMMA, LokiParser.TAG, LokiParser.CHUNK, LokiParser.ENTITY, LokiParser.MENTION, LokiParser.INCOMING, LokiParser.OUTGOING]:
-                self.state = 57
+                self.state = 55
                 self.attribute_constraint()
                 pass
             elif token in [LokiParser.NEGATED]:
-                self.state = 58
+                self.state = 56
                 self.negated_attribute_constraint()
                 pass
             else:
                 raise NoViableAltException(self)
 
-            self.state = 61
+            self.state = 59
             self.match(LokiParser.CLOSE_TC)
         except RecognitionException as re:
             localctx.exception = re
@@ -524,9 +517,9 @@ class LokiParser ( Parser ):
         self.enterRule(localctx, 14, self.RULE_negated_attribute_constraint)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 63
+            self.state = 61
             self.match(LokiParser.NEGATED)
-            self.state = 64
+            self.state = 62
             self.attribute_constraint()
         except RecognitionException as re:
             localctx.exception = re
@@ -573,11 +566,11 @@ class LokiParser ( Parser ):
         self.enterRule(localctx, 16, self.RULE_attribute_constraint)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 66
+            self.state = 64
             self.token_attribute()
-            self.state = 67
+            self.state = 65
             self.match(LokiParser.EQUAL)
-            self.state = 68
+            self.state = 66
             self.pattern()
         except RecognitionException as re:
             localctx.exception = re
@@ -620,17 +613,17 @@ class LokiParser ( Parser ):
         localctx = LokiParser.PatternContext(self, self._ctx, self.state)
         self.enterRule(localctx, 18, self.RULE_pattern)
         try:
-            self.state = 72
+            self.state = 70
             self._errHandler.sync(self)
             token = self._input.LA(1)
             if token in [LokiParser.SLASH]:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 70
+                self.state = 68
                 self.pattern_regex()
                 pass
             elif token in [LokiParser.QUOTE, LokiParser.LETTER]:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 71
+                self.state = 69
                 self.pattern_literal()
                 pass
             else:
@@ -680,11 +673,11 @@ class LokiParser ( Parser ):
         self.enterRule(localctx, 20, self.RULE_pattern_regex)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 74
+            self.state = 72
             self.match(LokiParser.SLASH)
-            self.state = 75
+            self.state = 73
             self.regex()
-            self.state = 76
+            self.state = 74
             self.match(LokiParser.SLASH)
         except RecognitionException as re:
             localctx.exception = re
@@ -729,21 +722,21 @@ class LokiParser ( Parser ):
         localctx = LokiParser.Pattern_literalContext(self, self._ctx, self.state)
         self.enterRule(localctx, 22, self.RULE_pattern_literal)
         try:
-            self.state = 83
+            self.state = 81
             self._errHandler.sync(self)
             token = self._input.LA(1)
             if token in [LokiParser.QUOTE]:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 78
+                self.state = 76
                 self.match(LokiParser.QUOTE)
-                self.state = 79
+                self.state = 77
                 self.literal()
-                self.state = 80
+                self.state = 78
                 self.match(LokiParser.QUOTE)
                 pass
             elif token in [LokiParser.LETTER]:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 82
+                self.state = 80
                 self.literal()
                 pass
             else:
@@ -790,18 +783,18 @@ class LokiParser ( Parser ):
         localctx = LokiParser.Inner_patternContext(self, self._ctx, self.state)
         self.enterRule(localctx, 24, self.RULE_inner_pattern)
         try:
-            self.state = 87
+            self.state = 85
             self._errHandler.sync(self)
-            la_ = self._interp.adaptivePredict(self._input,7,self._ctx)
+            la_ = self._interp.adaptivePredict(self._input,6,self._ctx)
             if la_ == 1:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 85
+                self.state = 83
                 self.literal()
                 pass
 
             elif la_ == 2:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 86
+                self.state = 84
                 self.regex()
                 pass
 
@@ -859,18 +852,18 @@ class LokiParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 90 
+            self.state = 88 
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while True:
-                self.state = 89
+                self.state = 87
                 _la = self._input.LA(1)
                 if not((((_la) & ~0x3f) == 0 and ((1 << _la) & ((1 << LokiParser.LETTER) | (1 << LokiParser.DIGIT) | (1 << LokiParser.PUNCT))) != 0)):
                     self._errHandler.recoverInline(self)
                 else:
                     self._errHandler.reportMatch(self)
                     self.consume()
-                self.state = 92 
+                self.state = 90 
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
                 if not ((((_la) & ~0x3f) == 0 and ((1 << _la) & ((1 << LokiParser.LETTER) | (1 << LokiParser.DIGIT) | (1 << LokiParser.PUNCT))) != 0)):
@@ -929,24 +922,24 @@ class LokiParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 94
+            self.state = 92
             self.match(LokiParser.LETTER)
-            self.state = 98
+            self.state = 96
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==LokiParser.DIGIT or _la==LokiParser.PUNCT:
-                self.state = 95
+                self.state = 93
                 _la = self._input.LA(1)
                 if not(_la==LokiParser.DIGIT or _la==LokiParser.PUNCT):
                     self._errHandler.recoverInline(self)
                 else:
                     self._errHandler.reportMatch(self)
                     self.consume()
-                self.state = 100
+                self.state = 98
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
-            self.state = 101
+            self.state = 99
             self.match(LokiParser.LETTER)
         except RecognitionException as re:
             localctx.exception = re
@@ -1007,7 +1000,7 @@ class LokiParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 103
+            self.state = 101
             _la = self._input.LA(1)
             if not((((_la) & ~0x3f) == 0 and ((1 << _la) & ((1 << LokiParser.WORD) | (1 << LokiParser.LEMMA) | (1 << LokiParser.TAG) | (1 << LokiParser.CHUNK) | (1 << LokiParser.ENTITY) | (1 << LokiParser.MENTION) | (1 << LokiParser.INCOMING) | (1 << LokiParser.OUTGOING))) != 0)):
                 self._errHandler.recoverInline(self)
